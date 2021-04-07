a64c61c575223ffcad7b9e913b40c106a558c749
ARM <subjectalpha32@gmail.com>
2021-04-08 00:21:14 +0500

    Integers :1234:

---
+++ WorkingWithNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WorkingWithNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, max, min;
+            float e;
+            a = 7;
+            b = 12;
+            c = a + b;
+            d = b / a;
+            e = b % a;
+            max = int.MaxValue;
+            min = int.MinValue;
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("The value of c is: " + c);
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("See the integer value when it should be fractional or decimal: " + d + "." + e);
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine($"The minimum and maximum values are as follows: \nMinValue: {min}\nMaxValue: {max}");
+            Console.WriteLine("-------------------------------------------------------");
+
+        }
+    }
+}
