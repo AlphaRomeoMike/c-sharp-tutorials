a30f9f0b4fec0a62daaad3f4dd8f69c89e6319a3
ARM <subjectalpha32@gmail.com>
2021-04-09 00:10:08 +0500

    Add project files.

---
+++ ObjectOrientedAbstraction/Animal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ObjectOrientedAbstraction
+{
+    public abstract class Animal
+    {
+        public abstract void animalSound();
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+        public static void Main()
+        {
+            Dog dog = new Dog();
+            dog.animalSound();
+        }
+        /*! At this point if we create an object of this class, 
+         * it will throw an error 
+         * The reason for this error is because this class is an 
+         * abstract class. Interfaces and Abstract classes cannot 
+         * have an object. To create an object, this class must be 
+         * inherited by another class
+         */
+    }
+
+    public class Dog : Animal
+        /*! At this point, we will have an error because we have 
+         * not yet implemented the method of animalSound. As soon
+         * as we do that, the error disappears
+         */
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("Animal says bow bow");
+            sleep();
+        }
+    }
+}
