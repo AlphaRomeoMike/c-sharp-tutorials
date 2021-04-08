888405b3f6838fa2a03e5c9f9cbd4e727bf4154d
ARM <subjectalpha32@gmail.com>
2021-04-08 23:49:54 +0500

    Add project files.

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        private string model = "Mustang";
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Model model = new Model();
+            //! This code throws an error because model attribute is private to Model
+            //! Console.WriteLine($"Model name is {model.car}");
+
+        }
+        public Program()
+        {
+            Console.WriteLine("A simple constructor:");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Model of car is " + model);
+            Console.WriteLine("---------------------------------------");
+        }
+    }
+
+    class Model
+    {
+        private string car = "Ford";
+    }
+}
