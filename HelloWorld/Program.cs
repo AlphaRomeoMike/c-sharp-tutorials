1c31417a82acdf3ac4437ea4c50875b87805442a
AlphaRomeoMike <rafaymodi18@gmail.com>
2021-06-24 19:15:33 +0500

    Added Nested Ifs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,36 +6,31 @@ namespace HelloWorld
     {
         static void Main()
         {
-            string FirstName = "Abdul Rafay";
-            string LastName = "Modi";
-            int FirstInteger = 23;
+            int x = 5, y = 20;
+            if(x > y)
+            {
+                if(x > y)
+                {
+                    Console.WriteLine("Value of x is greater than 10");
+                }
+                else
+                {
+                    Console.WriteLine("X is less than 10");
+                }
+            }
+            else
+            {
+                if(y <= 20)
+                {
+                    Console.WriteLine("Y is less than or equal to 20");
+                }
+                else
+                {
+                    Console.WriteLine("Y is greater than 20");
+                }
 
-            //to print a simple name
-            Console.WriteLine($"My firstname is {FirstName} and my lastname is {LastName} \n");
-            Console.WriteLine("---------------------------------------------------------------------");
-
-            //We can also write 
-            Console.WriteLine("My firstname is " + FirstName + " and my last name is " + LastName + "\n");
-            Console.WriteLine("---------------------------------------------------------------------");
-
-            //To print a simple integer we can do
-            Console.WriteLine("The first integer is " + FirstInteger.ToString() + "\n");
-            Console.WriteLine("---------------------------------------------------------------------");
-
-            //To find the length of a string we can simply use .Length property
-            Console.WriteLine($"The length of firstname is {FirstName.Length} \n");
-            Console.WriteLine("---------------------------------------------------------------------");
-
-            //To get the upper or lower case we can use 
-            Console.WriteLine($"The uppercase of firstname is {FirstName.ToUpper()} or lower case of firstname is {FirstName.ToLower()} \n");
-            Console.WriteLine("---------------------------------------------------------------------");
-
-            //find a word in a string
-            string SimpleSentence = "I am working with C# and I am loving it";
-            Console.WriteLine(SimpleSentence.Contains("working"));
-
-
-            Console.WriteLine("---------------------------------------------------------------------");
+                Console.WriteLine("Press enter to exit");
+            }
         }
     }
 }
