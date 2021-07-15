f3c60eb04a064c57b25816af5a1aa5cddec91c89
AlphaRomeoMike <rafaymodi18@gmail.com>
2021-07-15 10:18:59 +0500

    Add project files.

---
+++ User Defined Exception/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace User_Defined_Exception
+{
+    class LessThanZeroException : Exception
+    {
+        public LessThanZeroException(string message) :base(message)
+        {
+              
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -10;
+            try
+            {
+                if (a > 0)
+                {
+                    Console.WriteLine("Valid Data");
+                }
+                else
+                {
+                    throw new LessThanZeroException("You tried to divide by Zero");
+                }
+            }
+            catch (LessThanZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
